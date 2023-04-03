37daf547fec94cf0d2411da73dc0e7b8d8bbbc9f
Dang Hoang Phuc <dhp371.work@gmail.com>
2023-04-04 02:57:56 +0700

    fawjf

--- b6/duyetTheoChieuRong1.cpp
+++ b6/duyetTheoChieuRong1.cpp
@@ -14,6 +14,7 @@ void bfs(int visited[], int index, int **arr, int n)
         q.pop();
         for (int i = 0; i < n; i++)
         {
+            // Duyệt các đỉnh chưa được thăm. Nếu chưa có thì đẩy vào queue và set visited = true
             if (arr[v][i] == 1 && index != i && visited[i] == 0)
             {
                 q.push(i);
@@ -25,13 +26,18 @@ void bfs(int visited[], int index, int **arr, int n)
 
 int main()
 {
+    // So luong testcase
     int t;
     cin >> t;
+
     for (int k = 0; k < t; k++)
     {
+        // Số đỉnh của đồ thị
         int n;
         cin >> n;
+        
         int **arr = new int *[n];
+
         for (int i = 0; i < n; i++)
         {
             arr[i] = new int[n];
@@ -40,19 +46,27 @@ int main()
                 cin >> arr[i][j];
             }
         }
+
         for (int i = 0; i < n; i++)
         {
             int visited[n], first = 1;
+
+            // Đánh đấu row đó là đã duyệt qua
             for (int j = 0; j < n; j++)
             {
                 visited[j] = 0;
             }
+
             bfs(visited, i, arr, n);
+
             printf("From%4d can visit: ", i);
+
             for (int j = 0; j < n; j++)
             {
+                // Nếu kết quả từ bfs trả về là true => có thể đi
                 if (j != i && visited[j] == 1)
                 {
+                    // Nếu là số đầu thì không có dấu phẩy
                     if (first == 1)
                     {
                         first = 0;
@@ -64,12 +78,16 @@ int main()
                     printf("%3d", j);
                 }
             }
+
+            // Trường hợp không có cạnh nào liên kết
             if (first == 1)
             {
                 cout << "No vertex";
             }
+
             cout << endl;
         }
+
         cout << endl;
     }
 
